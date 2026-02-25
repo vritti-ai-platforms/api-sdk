a4d3ff1c4cdfa09eebf6b104cd18d032ffc4ad55
Shashank Raju <shashank@vrittiai.com>
2026-02-25 13:13:47 +0530

    feat(email): add ConfigModule import to EmailModule

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,8 +1,10 @@
 import { Global, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { EmailService } from './email.service';
 
 @Global()
 @Module({
+  imports: [ConfigModule],
   providers: [EmailService],
   exports: [EmailService],
 })
